445d8f93fb6dffc500d517496fb6e72027d37f13
Cristian Caloian <cristian.caloian@gmail.com>
2021-05-24 22:42:35 +0200

    Configure DAG

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -11,7 +11,14 @@ from helpers import SqlQueries
 
 default_args = {
     'owner': 'udacity',
+    'depends_on_past': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'email': ['cristian.caloian@gmail.com'],
+    'email_on_retry': False,
+    'email_on_fialure': False,
     'start_date': datetime(2019, 1, 12),
+    'catchup': False
 }
 
 dag = DAG('udac_example_dag',
@@ -63,3 +70,21 @@ run_quality_checks = DataQualityOperator(
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+start_operator >> stage_events_to_redshift
+start_operator >> stage_songs_to_redshift
+
+stage_events_to_redshift >> load_songplays_table
+stage_songs_to_redshift >> load_songplays_table
+
+load_songplays_table >> load_user_dimension_table
+load_songplays_table >> load_song_dimension_table
+load_songplays_table >> load_artist_dimension_table
+load_songplays_table >> load_time_dimension_table
+
+load_user_dimension_table >> run_quality_checks
+load_song_dimension_table >> run_quality_checks
+load_artist_dimension_table >> run_quality_checks
+load_time_dimension_table >> run_quality_checks
+
+run_quality_checks >> end_operator
